c72a93894dff14ecada18722223e10dadb86e649
Justin Kuchmy <Jukuchmy@gmail.com>
2026-03-15 15:29:54 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QListWidgetItem, QTreeWidgetItem, QSplitter
+from PyQt6.uic import loadUi
+from PyQt6.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Satisfactory Production Tree")
+        self.resize(800, 600)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
