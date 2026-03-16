f26db74f0d722376c2705e0ffc59ffcdc6fc4f15
Justin Kuchmy <Jukuchmy@gmail.com>
2026-03-16 09:47:30 +0800

    impl item list and on_item_clicked

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -3,6 +3,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QListWidgetItem, QTreeWid
 from PyQt6.uic import loadUi
 from PyQt6.QtCore import Qt
 import json
+from src.Item import Item
+from src.Resource import Resource
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,6 +16,11 @@ class MainWindow(QMainWindow):
             rawRecipes = json.load(file)
         with open("src/Data/Resources.json", "r") as file:
             rawResources = json.load(file)
+
+        self.items = [Item(**item) for item in rawRecipes]
+        self.rawResources = [Resource(**raw_mat) for raw_mat in rawResources]
+
+        self.loadItems()
 
         #connecting the ui elements to the python functions
         self.searchLineEdit.textChanged.connect(self.on_search_text_changed)
@@ -38,13 +45,26 @@ class MainWindow(QMainWindow):
         main_layout.setStretch(1, 1)  #center column width  (ratio. example 1/4, 25%)
         main_layout.setStretch(2, 2)  #right column width  (ratio. example 2/4, 50%)
 
-    def on_search_text_changed():
+    def loadItems(self):
+        index = 0
+        for i in range(len(self.items)):
+            item_obj = self.items[i]
+            display_name = item_obj.name.replace("_", " ")
+            list_item = QListWidgetItem(display_name)
+            list_item.setData(Qt.ItemDataRole.UserRole, {"obj": item_obj, "index": index})
+            index += 1
+            self.itemListWidget.addItem(list_item)
+
+    def on_search_text_changed(self):
         pass
-    def on_calculate_clicked():
+    def on_calculate_clicked(self):
         pass
-    def on_spinbox_changed():
+    def on_spinbox_changed(self):
         pass    
-    def on_item_clicked():
-        pass
-    def toggleCheckBox():
+    def on_item_clicked(self, list_item: QListWidgetItem):
+        item = list_item.data(Qt.ItemDataRole.UserRole)
+        label = "Selected Item: " + item["obj"].name
+        self.selectedItemLabel.setText(label)
+        self.selectedObject = item
+    def toggleCheckBox(self):
         pass
